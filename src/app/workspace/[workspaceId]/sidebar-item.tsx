6b4bedcd4f058820d5061d87588b7b030f72fcc9
Jun <peng940825@gmail.com>
2024-12-04 02:29:24 +0800

    Channels & members

---
+++ src/app/workspace/[workspaceId]/sidebar-item.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { LucideIcon } from "lucide-react";
+import { IconType } from "react-icons/lib";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const sidebarItemVariants = cva(
+  "flex items-center gap-1.5 justify-start font-normal h-7 px-[18px] text-sm overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "text-[#f9edffcc]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface SidebarItemProps {
+  id: string;
+  label: string;
+  icon: LucideIcon | IconType;
+  variant?: VariantProps<typeof sidebarItemVariants>["variant"];
+}
+
+const SidebarItem = ({ id, label, icon: Icon, variant }: SidebarItemProps) => {
+  const workspaceId = useWorkspaceId();
+
+  return (
+    <Button
+      size="sm"
+      variant="transparent"
+      className={cn(sidebarItemVariants({ variant }))}
+      asChild
+    >
+      <Link href={`/workspace/${workspaceId}/channel/${id}`}>
+        <Icon className="size-3.5 mr-1 shrink-0" />
+        <span className="text-sm truncate">{label}</span>
+      </Link>
+    </Button>
+  );
+};
+
+export default SidebarItem;
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,11 +1,22 @@
-import { Loader, AlertTriangle } from "lucide-react";
+import {
+  Loader,
+  HashIcon,
+  SendHorizonal,
+  AlertTriangle,
+  MessageSquareText,
+} from "lucide-react";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 
+import UserItem from "./user-item";
+import SidebarItem from "./sidebar-item";
 import WorkspaceHeader from "./workspace-header";
+import WorkspaceSection from "./workspace-section";
 
 const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -15,6 +26,12 @@ const WorkspaceSidebar = () => {
   });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
     id: workspaceId,
+  });
+  const { data: channels, isLoading: _channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+  const { data: members, isLoading: _membersLoading } = useGetMembers({
+    workspaceId,
   });
 
   if (memberLoading || workspaceLoading) {
@@ -40,6 +57,35 @@ const WorkspaceSidebar = () => {
         isAdmin={member.role === "admin"}
         workspace={workspace}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem id="threads" label="Threads" icon={MessageSquareText} />
+        <SidebarItem id="drafts" label="Drafts & Sent" icon={SendHorizonal} />
+      </div>
+      <WorkspaceSection label="Channels" hint="New channel" onNew={() => {}}>
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
+          />
+        ))}
+      </WorkspaceSection>
+
+      <WorkspaceSection
+        label="Direct Messages"
+        hint="New direct message"
+        onNew={() => {}}
+      >
+        {members?.map((item) => (
+          <UserItem
+            key={item._id}
+            id={member._id}
+            label={item.user.name}
+            image={item.user.image}
+          />
+        ))}
+      </WorkspaceSection>
     </div>
   );
 };
